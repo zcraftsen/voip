6ba871456edb2f3bd548ddb1dd1e779e2cc1b563
zcraftsen <59503436+zcraftsen@users.noreply.github.com>
2024-08-20 14:49:28 +0800

    Create FPBXv16_NTP_ww.sh

---
+++ FPBXv16_NTP_ww.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# File to be modified
+ntp_conf="/etc/ntp.conf"
+
+# Comment out the specified lines
+sed -i 's/^server 0.centos.pool.ntp.org iburst/#&/' "$ntp_conf"
+sed -i 's/^server 1.centos.pool.ntp.org iburst/#&/' "$ntp_conf"
+sed -i 's/^server 2.centos.pool.ntp.org iburst/#&/' "$ntp_conf"
+sed -i 's/^server 3.centos.pool.ntp.org iburst/#&/' "$ntp_conf"
+
+# Add new NTP server entries
+echo -e "\n# added\nserver ntp1.tls.ad iburst\nserver ntp3.tls.ad iburst" >> "$ntp_conf"
+
+# Optional: Restart the NTP service to apply changes
+systemctl restart ntpd
